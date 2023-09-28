bb522eb234d245fc0393d0354db425e006c2b36e
Peter Grajcar <petergrajcar@duck.com>
2023-09-28 22:28:46 +0200

    initial commit
    Signed-off-by: Peter Grajcar <petergrajcar@duck.com>

---
+++ model.py
@@ -0,0 +1,78 @@
+import flax.linen as nn
+import jax.numpy as jnp
+import numpy as np
+import jax
+
+class SinusoidalEmbedding(nn.Module):
+    dim: int
+
+    @nn.compact
+    def __call__(self, inputs):
+        i = jnp.arange(self.dim // 2)
+        sin_embeddings = jnp.sin(2 * np.pi * inputs / 20 ** (2 * i / self.dim))
+        cos_embeddings = jnp.cos(2 * np.pi * inputs / 20 ** (2 * i / self.dim))
+        embeddings = jnp.concatenate([sin_embeddings, cos_embeddings], axis=-1)
+        return embeddings
+
+
+class ResidualBlock(nn.Module):
+    width: int
+
+    @nn.compact
+    def __call__(self, inputs, noise_embeddings, train=True):
+        residual = nn.Conv(self.width, (1, 1))(inputs) if self.width != inputs.shape[-1] else inputs
+
+        hidden = nn.BatchNorm(use_running_average=not train)(inputs)
+        hidden = nn.swish(hidden)
+        hidden = nn.Conv(self.width, (3, 3), padding="SAME", use_bias=False)(hidden)
+
+        noise_embeddings = nn.Dense(self.width)(noise_embeddings)
+        noise_embeddings = nn.swish(noise_embeddings)
+        hidden += noise_embeddings
+
+        hidden = nn.BatchNorm(use_running_average=not train)(hidden)
+        hidden = nn.swish(hidden)
+        hidden = nn.Conv(self.width, (3, 3), padding="SAME", use_bias=False, kernel_init=nn.initializers.zeros_init())(hidden)
+
+        hidden += residual
+        return hidden
+
+
+class DiffusionModel(nn.Module):
+    stages: int
+    stage_blocks: int
+    channels: int
+    out_channels: int = 3
+
+    @nn.compact
+    def __call__(self, inputs, conditioning, noise_rates, train=True):
+        noise_embeddings = SinusoidalEmbedding(self.channels)(noise_rates)
+
+        hidden = nn.Conv(self.channels, (3, 3), padding="SAME")(inputs)
+
+        outputs = []
+        for i in range(self.stages):
+            for _ in range(self.stage_blocks):
+                hidden = ResidualBlock(self.channels << i)(hidden, noise_embeddings, train)
+                outputs.append(hidden)
+
+            hidden = nn.Conv(self.channels << (i + 1), (3, 3), strides=(2, 2), padding="SAME")(hidden)
+
+        for _ in range(self.stage_blocks):
+            hidden = ResidualBlock(self.channels << self.stages)(hidden, noise_embeddings, train)
+
+        for i in reversed(range(self.stages)):
+            hidden = nn.ConvTranspose(self.channels << i, (4, 4), strides=(2, 2), padding="SAME")(hidden)
+
+            for _ in range(self.stage_blocks):
+                hidden = jnp.concatenate([hidden, outputs.pop()], axis=-1)
+                hidden = ResidualBlock(self.channels << i)(hidden, noise_embeddings, train)
+
+        assert len(outputs) == 0
+
+        outputs = nn.BatchNorm(use_running_average=not train)(hidden)
+        outputs = nn.swish(outputs)
+        outputs = nn.Conv(self.out_channels, (3, 3), padding="SAME", kernel_init=nn.initializers.zeros_init())(outputs)
+
+        return outputs
+
---
+++ train.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python3
+import jax
+import optax
+import argparse
+import jax.numpy as jnp
+from model import DiffusionModel
+from pprint import pprint
+from flax.training import train_state
+from typing import Any, Dict
+from tqdm.auto import tqdm
+from functools import partial
+
+parser = argparse.ArgumentParser()
+# These arguments will be set appropriately by ReCodEx, even if you change them.
+parser.add_argument("--batch_size", default=64, type=int, help="Batch size.")
+parser.add_argument("--channels", default=32, type=int, help="CNN channels in the first stage.")
+parser.add_argument("--dataset", default="oxford_flowers102", type=str, help="Image64 dataset to use.")
+parser.add_argument("--debug", default=False, action="store_true", help="If given, run functions eagerly.")
+parser.add_argument("--downscale", default=8, type=int, help="Conditional downscale factor.")
+parser.add_argument("--ema", default=0.999, type=float, help="Exponential moving average momentum.")
+parser.add_argument("--epoch_batches", default=1_000, type=int, help="Batches per epoch.")
+parser.add_argument("--epochs", default=100, type=int, help="Number of epochs.")
+parser.add_argument("--loss", default="MeanAbsoluteError", type=str, help="Loss object to use.")
+parser.add_argument("--plot_each", default=None, type=int, help="Plot generated images every such epoch.")
+parser.add_argument("--sampling_steps", default=50, type=int, help="Sampling steps.")
+parser.add_argument("--seed", default=42, type=int, help="Random seed.")
+parser.add_argument("--stage_blocks", default=2, type=int, help="ResNet blocks per stage.")
+parser.add_argument("--stages", default=4, type=int, help="Stages to use.")
+
+args = parser.parse_args([] if "__file__" not in globals() else None)
+
+model = DiffusionModel(
+    stages=args.stages,
+    stage_blocks=args.stage_blocks,
+    channels=args.channels,
+)
+
+key = jax.random.PRNGKey(args.seed)
+key, init_key = jax.random.split(key)
+
+variables = model.init(init_key, jnp.ones([1, 64, 64, 3]), jnp.ones([1, 64, 64, 3]), jnp.ones([1, 1, 1, 1]), train=False)
+params = variables["params"]
+batch_stats = variables["batch_stats"]
+
+class TrainState(train_state.TrainState):
+    batch_stats: Any
+    key: jax.random.PRNGKey
+    ema_variables: Dict
+
+key, train_key = jax.random.split(key)
+state = TrainState.create(
+    apply_fn=model.apply,
+    params=params,
+    tx=optax.adamw(1e-3, b1=0.9, b2=0.999, eps=1e-8),
+    batch_stats=batch_stats,
+    key=train_key,
+    ema_variables=variables.copy().unfreeze(),
+    ema_momentum=args.ema,
+)
+
+
+@jax.jit
+def diffusion_rates(times: jnp.ndarray):
+    starting_angle, final_angle = 0.025, jnp.pi / 2 - 0.025
+    signal_rates = jnp.cos((1 - times) * starting_angle + times * final_angle)[..., None, None, None]
+    noise_rates = jnp.sin((1 - times) * starting_angle + times * final_angle)[..., None, None, None]
+    return signal_rates, noise_rates
+
+
+@jax.jit
+def normalise_images(images: jnp.ndarray, mean: float, std: float):
+    return (images - mean) / std
+
+
+@jax.jit
+def denormalise_images(images: jnp.ndarray, mean: float, std: float):
+    images = images * std + mean
+    images = jnp.clip(images, 0, 255)
+    return images.astype(jnp.uint8)
+
+
+@jax.jit
+def train_step(state: TrainState, batch: tuple[jnp.ndarray, jnp.ndarray]):
+    images, conditioning = batch
+    images = normalise_images(images, ..., ...)
+
+    key, noise_key, time_key = jax.random.split(state.key, 3)
+    state = state.replace(key=key)
+    noises = jax.random.normal(noise_key, images.shape[0])
+    times = jax.random.uniform(time_key, images.shape[:1])
+
+    signal_rates, noise_rates = diffusion_rates(times)
+    noisy_images = signal_rates * images + noise_rates * noises
+
+    variables = {"params": state.params, "batch_stats": state.batch_stats}
+
+    def loss_fn(params):
+        predicted_noises, updates = state.apply_fn(
+            variables,
+            noisy_images,
+            conditioning,
+            noise_rates,
+            train=True,
+        )
+        loss = optax.squared_error(noises, predicted_noises).mean()
+        return loss, (predicted_noises, updates)
+
+    # Gradient update
+    grad_fn = jax.value_and_grad(loss_fn, has_aux=True)
+    (loss, (predicted_noises, updates)), grads = grad_fn(state.params)
+    state = state.apply_gradients(grads=grads)
+    state = state.replace(batch_stats=updates["batch_stats"])
+
+    # Exponential moving average update
+    ema_variables, ema_variables_treedef = jax.tree_util.tree_flatten(state.ema_variables)
+    variables, _ = jax.tree_util.tree_flatten(variables)
+
+    @partial(jax.pmap)
+    def ema_update(ema_variable, variable):
+        return ema_variable * state.ema_momentum + variable * (1 - state.ema_momentum)
+    ema_update(ema_variables, variables)
+
+    ema_variables = jax.tree_util.tree_unflatten(ema_variables_treedef, ema_variables)
+    state = state.replace(ema_variables=ema_variables)
+
+    return state, loss
+
